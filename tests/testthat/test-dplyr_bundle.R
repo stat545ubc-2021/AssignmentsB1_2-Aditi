83bbf25df563985444b317d56f9fe3d190676ece
Aditi Nagaraj Nallan <aditi48nagaraj@gmail.com>
2021-11-20 11:31:44 -0800

    Edits

--- tests/testthat/test-dplyr_bundle.R
+++ tests/testthat/test-dplyr_bundle.R
@@ -1,9 +1,11 @@
+#The first test shows that having NA values distributed between the different categories still provides an output and in the expected format- in the form of a tibble. The first test is a two-in-one test that test for NA values and output type
 test_that("Function dplyr_bundle outputs a tibble", {
   #creating a simple input data to test function
   input <- data.frame(x = c("a","a","a", "b", "b","b","c","c","c"),
                       y = c(1,NA,3,NA,5,7,9,NA,11))
   #testing that the output is a tibble
   expect_s3_class(dplyr_bundle(input, group = x, summary = y), "tbl")})
+
 
 test_that("Incorrect column types throw an error.", {
   #testing that providing character column for numeric calculations will throw an error
@@ -14,5 +16,10 @@ test_that("Incorrect column types throw an error.", {
 test_that("A column of only NAs for numeric variable throws an error", {
   input <- data.frame(x = c("a","a","a", "b", "b","b","c","c","c"),
                       y = c(NA,NA,NA,NA,NA,NA,NA,NA,NA))
-  expect_error(grouping_summarising(input, group = x, summary = y))
+  expect_error(dplyr_bundle(input, group = x, summary = y))
+})
+
+test_that("Output returned is visible", {
+  result = dplyr_bundle(gapminder::gapminder, group = continent, summary = lifeExp)
+  expect_visible(result)
 })
